cd60f5bc4c60d20ee252e05eeadc5ddc9d0181e0
hthcrwzy <119731650+hthcrwzy@users.noreply.github.com>
2022-12-22 19:44:06 +0900

    例を改善

--- tokens/tokens.go
+++ tokens/tokens.go
@@ -1,23 +1,23 @@
 package tokens
 
 const (
-	// インクリメント
+	// インクリメント +
 	SEMICOLON = ';'
-	// デクリメント
+	// デクリメント -
 	COLON = ':'
 
-	// ポインタをインクリメント
+	// ポインタをインクリメント >
 	BANG = '!'
-	// ポインタをデクリメント
+	// ポインタをデクリメント <
 	I = 'i'
 
-	// 繰り返し 開始点
+	// 繰り返し 開始点 [
 	LEFT_ANGLE_BRACKET = '<'
-	// 繰り返し 終了点
+	// 繰り返し 終了点 ]
 	RIGHT_ANGEL_BRACKET = '>'
 
-	// I/O O
+	// I/O O .
 	J = 'j'
-	// I/O I
+	// I/O I ,
 	QUESTION = '?'
 )
